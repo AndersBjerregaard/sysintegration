9cfcb76d1908221938996af007fb0d0459bbc5ad
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2023-09-21 23:57:55 +0200

    done topic receiver for email-service

--- Tours/email-service/src/main.rs
+++ Tours/email-service/src/main.rs
@@ -1,4 +1,4 @@
-use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Result};
+use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Result, ExchangeDeclareOptions};
 
 fn main() -> Result<()> {
     // Open connection.
@@ -7,19 +7,43 @@ fn main() -> Result<()> {
     // Open a channel - None says let the library choose the channel ID.
     let channel = connection.open_channel(None)?;
 
-    // Declare the "hello" queue.
-    let queue = channel.queue_declare("hello", QueueDeclareOptions::default())?;
+    // Declare the exchange.
+    let exchange = channel.exchange_declare(
+        amiquip::ExchangeType::Topic, 
+        "bookings", 
+        ExchangeDeclareOptions::default()
+    )?;
 
-    // Start a consumer.
-    let consumer = queue.consume(ConsumerOptions::default())?;
-    println!("Waiting for messages. Press Ctrl-C to exit.");
+    // Declare the exclusive, server-named queue to use to consume.
+    let queue = channel.queue_declare(
+        "",
+        QueueDeclareOptions {
+            exclusive: true,
+            ..QueueDeclareOptions::default()
+        },
+    )?;
+    println!("created exclusive queue {}", queue.name());
+
+    queue.bind(
+        &exchange, 
+        "tour.book", 
+        amiquip::FieldTable::new()
+    )?;
+
+    // Start a consumer. Use no_ack: true so the server doesn't wait
+    // for this app to ack the message it sends.
+    let consumer = queue.consume(ConsumerOptions { 
+        no_ack: true,
+        ..ConsumerOptions::default()
+    })?;
+
+    println!("Waiting for bookings. Press Ctrl+C to exit.");
 
     for (i, message) in consumer.receiver().iter().enumerate() {
         match message {
             ConsumerMessage::Delivery(delivery) => {
                 let body = String::from_utf8_lossy(&delivery.body);
-                println!("({:>3}) Received [{}]", i, body);
-                consumer.ack(delivery)?;
+                println!("({:>3}) {}:{}", i, delivery.routing_key, body);
             }
             other => {
                 println!("Consumer ended: {:?}", other);
