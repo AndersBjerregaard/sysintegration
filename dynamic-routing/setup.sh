f5d1855143c74c310205ac70c724614da70143b8
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2023-11-02 11:36:32 +0100

    wip: producer src and containerization

--- dynamic-routing/setup.sh
+++ dynamic-routing/setup.sh
@@ -8,9 +8,10 @@ echo "Waiting for RabbitMQ management API to be ready..."
 
 sleep 3
 
-docker exec rabbitmq rabbitmqctl status
+docker exec rabbitmq rabbitmqctl status > /dev/null
 
 # Source configuration file to load variables
 source rabbit-infrastructure.cfg
 
-rabbitmqadmin declare queue name="$PROD_TO_BALANCER_QUEUE_NAME" durable=false
+echo "Declaring queue '${PROD_TO_BALANCER_QUEUE_NAME}' with options 'durable=false'"
+docker exec ${RABBITMQ_CONTAINER_NAME} rabbitmqadmin declare queue name="${PROD_TO_BALANCER_QUEUE_NAME}" durable=false
