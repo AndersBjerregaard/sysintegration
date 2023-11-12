daf5c3fe8e3b5df14d7d8f5f59ed21ee4c29d6e0
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2023-11-12 13:17:38 +0100

    Added error handling, panics and default environment variables

--- dynamic-routing/producer/src/main.rs
+++ dynamic-routing/producer/src/main.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::env;
 
 use lapin::{ConnectionProperties, Connection};
@@ -6,6 +7,14 @@ use lapin::{ConnectionProperties, Connection};
 #[tokio::main]
 async fn main() {
     let uri: String = env::var("RABBITMQ_URI").unwrap();
+    println!("[Info] RabbitMQ uri was loaded as `{}`", &uri);
+
+    let exchange_name: String = env::var("P_EXCH_NAME").unwrap();
+    if exchange_name == "not set" {
+        panic!("[Critical] Environment variable `P_EXCH_NAME` was not set! Aborting...");
+    } else {
+        print!("[Info] Environment variable `P_EXCH_NAME` was loaded as `{}`", exchange_name);
+    }
 
     let options: ConnectionProperties = ConnectionProperties::default()
         .with_connection_name("producer_connection".to_string().into())
@@ -17,16 +26,23 @@ async fn main() {
 
 async fn connect_rabbitmq(uri: &str, options: ConnectionProperties) -> Connection {
     let mut connection: Result<Connection, lapin::Error> = Connection::connect(uri, options.clone()).await;
-
+    let mut interval = 3;
+    let mut retries = 0;
 
     while connection.is_err() {
+        if retries == 10 {
+            core::panic!("[Critical] Failed to connect to RabbitMQ after 10 retries! Aborting...");
+        }
         println!(
-            "--> Failed to connect to rabbitmq: {}",
+            "[Error] Failed to connect to RabbitMQ: {}",
             &connection.unwrap_err()
         );
+        println!("[Info] Attempting to reconnect after {} seconds", interval);
+        interval += 2;
+        retries += 1;
+        
         connection = Connection::connect(uri, options.clone()).await;
     }
 
-    let connection: Connection = connection.unwrap();
-    connection
+    connection.unwrap()
 }
